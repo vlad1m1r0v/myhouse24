a3e1ce96c022984afef490d58e841420297dcc52
ant100pshnik <mypochtav@gmail.com>
2024-09-23 13:45:15 +0300

    add services page on website

--- src/website_management/views/website/__init__.py
+++ src/website_management/views/website/__init__.py
@@ -1,2 +1,3 @@
 from .home import WebsiteHomeView
 from .about_us import WebsiteAboutUsView
+from .services import WebsiteServicesView
---
+++ src/website_management/views/website/services.py
@@ -0,0 +1,21 @@
+from django.core.paginator import Paginator
+from django.views.generic import TemplateView
+
+from src.website_management.models import ServicesPage, ServicesPageBlock
+
+
+class WebsiteServicesView(TemplateView):
+    template_name = 'website/services.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['page'] = ServicesPage.objects.first()
+
+        services = ServicesPageBlock.objects.all()
+        paginator = Paginator(services, per_page=1)
+        page_number = self.request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+        context['page_obj'] = page_obj
+
+        return context
